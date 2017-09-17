d27da2833703a48d649ad634ffc50ea9fd707d50
aschouas <l>
2017-09-18 03:17:41 +0800

    Site updated: 2017-09-18 03:17:39

--- Decathlon/assets/js/app.js
+++ Decathlon/assets/js/app.js
@@ -13,5 +13,57 @@
   });
 })(jQuery);
 
-
+var baseUrl="localhost:8080/bxs/"
 //
+
+
+$("#loginbuttton").click(function(){
+	
+	var pass=$("#user-pass").val();
+	
+	var name=$("#user-name").val();
+	
+	if(pass.length==0){
+		$("#user-pass")[0].placeholder="请输入密码"
+		return
+	}
+	if(name.length==0){
+		$("#user-name")[0].placeholder="请输入账号"
+		return
+	}
+	
+
+	if (name=='tom' && pass=='123456') {
+		//总经理
+		window.location.href="total.html";
+	} else if (name=='jerry' && pass=='123456') {
+		//区域经理
+		window.location.href="region.html";
+	} else if (name=='jack' && pass=='123456') {
+		//门店
+		window.location.href="index.html";
+	} else {
+		//没有权限
+		confirm("你没有权限")
+	}
+
+
+
+	
+	// $.ajax({
+			// type: "POST",
+             // url: "http://localhost:8080/bxs/login",
+             // data: {uname:name,passwd:pass},
+             // dataType: "JSON",
+             // success: function(data){
+				// self.location='admin-table.html'; 
+				// window.location.href='admin-table.html';
+			 // },
+			 // error: function () {
+				// console.log("error");
+			// }
+		// })
+	
+	
+	
+})
